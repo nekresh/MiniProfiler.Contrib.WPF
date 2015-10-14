3650e2036594d0f491660657fceabfefa2d51239
nekresh <nekresh@gmail.com>
2015-10-14 11:26:15 +0200

    Limit name size to 150 characters

---
+++ MiniProfiler.Contrib.WPF/Converter/MaxLengthConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace MiniProfiler.Contrib.WPF.Converter
+{
+    public class MaxLengthConverter : IValueConverter
+    {
+        public int MaxLength { get; set; }
+
+        public MaxLengthConverter()
+        {
+            MaxLength = 150;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value== null ? string.Empty : value.ToString();
+
+            return str.Length > MaxLength ? str.Substring(0, MaxLength) + "..." : str;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
